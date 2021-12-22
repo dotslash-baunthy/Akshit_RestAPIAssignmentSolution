ac90ae2a96e00d442cad028b5c7fbaade9a47867
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-22 15:31:13 +0530

    Update EmployeeDeleteController.java
    Annotate method argument for delete by ID with @RequestParam

--- src/main/java/com/akshitbaunthy/restapiassignmentsolution/controller/EmployeeDeleteController.java
+++ src/main/java/com/akshitbaunthy/restapiassignmentsolution/controller/EmployeeDeleteController.java
@@ -4,6 +4,7 @@ import com.akshitbaunthy.restapiassignmentsolution.service.EmployeeDeleteService
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +19,7 @@ public class EmployeeDeleteController {
     }
 
     @GetMapping("/single")
-    public String deleteById(Long id) {
+    public String deleteById(@RequestParam Long id) {
         return employeeDeleteService.deleteById(id);
     }
 
