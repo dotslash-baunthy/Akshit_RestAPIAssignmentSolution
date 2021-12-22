3c471b9e872d44aeb77c59357d620e54b451a295
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-22 17:22:42 +0530

    Update data.sql
    Insert into role

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ INSERT INTO EMPLOYEE
 VALUES (1, 'akshit.baunthy@gmail.com', 'Akshit', 'Baunthiyal');
 INSERT INTO EMPLOYEE
 VALUES (2, 'ananya98baunthiyal@gmail.com', 'Ananya', 'Baunthiyal');
+
+INSERT INTO ROLE
+VALUES (1, 'ADMIN')
