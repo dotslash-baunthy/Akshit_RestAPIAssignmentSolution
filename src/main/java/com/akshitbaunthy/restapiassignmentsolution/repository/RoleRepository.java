06b55e753ab0a7e13dcb07c47c10872d86832af4
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-28 21:01:09 +0530

    Create repository interfaces

--- src/main/java/com/akshitbaunthy/restapiassignmentsolution/repository/RoleRepository.java
+++ src/main/java/com/akshitbaunthy/restapiassignmentsolution/repository/RoleRepository.java
@@ -2,8 +2,7 @@ package com.akshitbaunthy.restapiassignmentsolution.repository;
 
 import com.akshitbaunthy.restapiassignmentsolution.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Integer> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
 }
--- src/main/java/com/akshitbaunthy/restapiassignmentsolution/repository/UserRepository.java
+++ src/main/java/com/akshitbaunthy/restapiassignmentsolution/repository/UserRepository.java
@@ -4,8 +4,7 @@ import com.akshitbaunthy.restapiassignmentsolution.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    @Query("select u from User u where u.username = ?1")
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
     public User getUserByUsername(String username);
 }
