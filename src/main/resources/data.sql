744382ea82f16f7b249ed7b76ce03718637a76b8
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-28 21:02:31 +0530

    Update data.sql
    Add data into all tables (employee, user, role and users_roles)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,17 +3,17 @@ VALUES (1, 'akshit.baunthy@gmail.com', 'Akshit', 'Baunthiyal');
 INSERT INTO EMPLOYEE
 VALUES (2, 'ananya98baunthiyal@gmail.com', 'Ananya', 'Baunthiyal');
 
--- INSERT INTO ROLE
--- VALUES (1, 'ADMIN');
--- INSERT INTO ROLE
--- VALUES(2, 'USER');
---
--- INSERT INTO USER
--- VALUES (1, TRUE, '$2a$12$wLrHcTJUW6BIKxylLNneXezJp5xUlewueLBcy/OfBbyLVM7QB.HEG', 'akshit');
--- INSERT INTO USER
--- VALUES(2, TRUE, '$2a$12$1bjI/nmgK.pLBiZKKE.9yeQYFa3aXzJJRiRYCDWewDaLXtCJikJQC', 'ananya');
---
--- INSERT INTO USERS_ROLES
--- VALUES(1, 1);
--- INSERT INTO USERS_ROLES
--- VALUES(2,2);
+INSERT INTO ROLE
+VALUES (1, 'ADMIN');
+INSERT INTO ROLE
+VALUES(2, 'USER');
+
+INSERT INTO USER
+VALUES (1, '$2a$12$wLrHcTJUW6BIKxylLNneXezJp5xUlewueLBcy/OfBbyLVM7QB.HEG', 'akshit');
+INSERT INTO USER
+VALUES(2, '$2a$12$1bjI/nmgK.pLBiZKKE.9yeQYFa3aXzJJRiRYCDWewDaLXtCJikJQC', 'ananya');
+
+INSERT INTO USERS_ROLES
+VALUES(1, 1);
+INSERT INTO USERS_ROLES
+VALUES(2,2);
