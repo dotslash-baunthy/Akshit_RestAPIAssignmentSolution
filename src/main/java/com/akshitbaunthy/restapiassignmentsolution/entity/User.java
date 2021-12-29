c9c0c4dab4a895d89ba7bd07b21c090630d66c65
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-29 17:44:51 +0530

    Update User.java
    Change cascade type to merge. Other cascade types are creating issues.
    ALL results in adding new roles
    PERSIST results in an error

--- src/main/java/com/akshitbaunthy/restapiassignmentsolution/entity/User.java
+++ src/main/java/com/akshitbaunthy/restapiassignmentsolution/entity/User.java
@@ -22,7 +22,7 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "User_id"),
